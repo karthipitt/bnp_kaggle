5af448af89f7bedb924d391c86694039bc70546f
karthipitt <kas389@pitt.edu>
2016-02-09 14:25:22 -0500

    Titanic ML

---
+++ titanic_fun.R
@@ -0,0 +1,40 @@
+
+readData <- function(path.name, file.name, column.types, missing.types) {
+  read.csv( url( paste(path.name, file.name, sep="") ), 
+            colClasses=column.types,
+            na.strings=missing.types )
+}
+
+setwd("~/Dropbox/DS_Spring2016")
+Titanic.path <- "https://raw.github.com/wehrley/Kaggle_Titanic/master/"
+train.data.file <- "train.csv"
+test.data.file <- "test.csv"
+missing.types <- c("NA", ""
+)
+train.column.types <- c('integer',   # PassengerId
+'factor',    # Survived
+'factor',    # Pclass
+'character', # Name
+'factor',    # Sex
+'numeric',   # Age
+'integer',   # SibSp
+'integer',   # Parch
+'character', # Ticket
+'numeric',   # Fare
+'character', # Cabin
+'factor'     # Embarked
+)
+test.column.types <- train.column.types[-2]
+train.raw <- readData(Titanic.path, train.data.file,
+train.column.types, missing.types)
+df.train <- train.raw
+
+test.raw <- readData(Titanic.path, test.data.file, 
+                     test.column.types, missing.types)
+df.infer <- test.raw 
+
+require(Amelia)
+missmap(df.train, main="Titanic Training Data - Missings Map", 
+        col=c("yellow", "black"), legend=FALSE)
+
+
